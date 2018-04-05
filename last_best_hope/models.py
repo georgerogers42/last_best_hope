088f811fa2e4dff243098081b488c744ddbfd9e8
George Rogers <george.f.rogers@gmail.com>
2018-04-05 15:38:34 -0500

    update

--- last_best_hope/models.py
+++ last_best_hope/models.py
@@ -2,6 +2,8 @@ from os import environ
 from contextlib import contextmanager
 from datetime import datetime
 
+import arrow
+
 from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -39,4 +41,16 @@ class Page(Base, Encodable):
     title = Column(String, nullable=False)
     contents = Column(Text, nullable=False)
     ctime = Column(DateTime, nullable=False, default=datetime.now)
+    @property
+    def arrow_ctime(self):
+        return arrow.get(self.ctime)
+    @arrow_ctime.set
+    def arrow_ctime(self, o):
+        self.ctime = o.datetime
     utime = Column(DateTime, nullable=False, default=datetime.now)
+    @property
+    def arrow_utime(self):
+        return arrow.get(self.utime)
+    @arrow_utime.set
+    def arrow_utime(self, o):
+        self.ctime = o.datetime
