20e40b36b3fe5fa67ce191a3c9bf5d789d47d4ac
George Rogers <george.f.rogers@gmail.com>
2018-04-06 09:53:24 -0500

    update

--- last_best_hope/web.py
+++ last_best_hope/web.py
@@ -6,6 +6,12 @@ app = application = Flask(__name__)
 app.json_encoder = ejson.Encoder
 app.register_blueprint(api.blueprint, url_prefix="/api")
 
+def page_by_slug(s, slug):
+    page = s.query(models.Page).filter(slug == models.Page.slug).first()
+    if page == None:
+        page = models.Page(slug=slug, title=slug, contents="", utime=arrow.get().datetime)
+    return page
+
 @app.route("/", methods=["GET"])
 def get_index():
     return get_page("index")
@@ -13,17 +19,13 @@ def get_index():
 @app.route("/page/<path:slug>", methods=["GET"])
 def get_page(slug):
     with models.session() as s:
-        page = s.query(models.Page).filter(slug == models.Page.slug).first()
-        if page == None:
-            page = models.Page(slug=slug, title=slug, contents="", utime=arrow.get().datetime)
+        page = page_by_slug(s, slug)
         return render_template("app/index.html", page=page)
 
 @app.route("/edit/page/<path:slug>", methods=["GET"])
 def get_page_form(slug):
     with models.session() as s:
-        page = s.query(models.Page).filter(slug == models.Page.slug).first()
-        if page == None:
-            page = models.Page(slug=slug, title=slug, contents="", utime=arrow.get().datetime)
+        page = page_by_slug(s, slug)
         return render_template("app/edit.html", page=page)
 
 @app.route("/edit/page/<path:slug>", methods=["POST"])
